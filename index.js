ed41a79279517ffaeba32bd7796aa558823ceeca
StrangeOnFire <120164506+StrangeOnFire@users.noreply.github.com>
2023-05-04 23:23:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    reset:true,
+    duration:2500,
+    distance:"50px",
+    delay:400
+})
+
+ScrollReveal().reveal('.top-container-img', {origin:'right',delay:500});
+ScrollReveal().reveal('.top-container-text-h4', {origin:'left'});
+ScrollReveal().reveal('.top-container-text-heading', {origin:'bottom',delay:300});
+ScrollReveal().reveal('.sc1', {origin:'bottom',delay:300});
+ScrollReveal().reveal('.sc2', {origin:'left',delay:400 , interval:200});
+ScrollReveal().reveal('.items-img', {origin:'top',delay:200, interval:200});
+ScrollReveal().reveal('.items-des', {origin:'bottom',delay:300, interval:200});
